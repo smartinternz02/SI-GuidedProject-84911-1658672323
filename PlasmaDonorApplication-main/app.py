20c8b49d56743d02333b7e129ceb8b2bc4efc60c
sunilkjoseph <104191368+sunilkjoseph@users.noreply.github.com>
2022-07-30 22:00:30 +0530

    Add files via upload

---
+++ PlasmaDonorApplication-main/app.py
@@ -0,0 +1,106 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import ibm_db
+import json
+app = Flask(__name__)
+
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=2d46b6b4-cbf6-40eb-bbce-6251e6ba0300.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=32328;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=rzp60332;PWD=pQKhhRttcrGEyBKA",'','')
+
+@app.route('/registration')
+def home():
+    return render_template('register.html')
+
+@app.route('/register',methods=['POST'])
+def register():
+    x = [x for x in request.form.values()]
+    print(x)
+    name=x[0]
+    email=x[1]
+    phone=x[2]
+    city=x[3]
+    infect=x[4]
+    blood=x[5]
+    password=x[6]
+    sql = "SELECT * FROM user WHERE email =?"
+    stmt = ibm_db.prepare(conn, sql)
+    ibm_db.bind_param(stmt,1,email)
+    ibm_db.execute(stmt)
+    account = ibm_db.fetch_assoc(stmt)
+    print(account)
+    if account:
+        return render_template('register.html', pred="You are already a member, please login using your details")
+    else:
+        insert_sql = "INSERT INTO  user VALUES (?, ?, ?, ?, ?, ?, ?)"
+        prep_stmt = ibm_db.prepare(conn, insert_sql)
+        ibm_db.bind_param(prep_stmt, 1, name)
+        ibm_db.bind_param(prep_stmt, 2, email)
+        ibm_db.bind_param(prep_stmt, 3, phone)
+        ibm_db.bind_param(prep_stmt, 4, city)
+        ibm_db.bind_param(prep_stmt, 5, infect)
+        ibm_db.bind_param(prep_stmt, 6, blood)
+        ibm_db.bind_param(prep_stmt, 7, password)
+        ibm_db.execute(prep_stmt)
+        return render_template('register.html', pred="Registration Successful, please login using your details")
+       
+           
+        
+
+@app.route('/')    
+@app.route('/login')
+def login():
+    return render_template('login.html')
+    
+@app.route('/loginpage',methods=['POST'])
+def loginpage():
+    user = request.form['user']
+    passw = request.form['passw']
+    sql = "SELECT * FROM user WHERE email =? AND password=?"
+    stmt = ibm_db.prepare(conn, sql)
+    ibm_db.bind_param(stmt,1,user)
+    ibm_db.bind_param(stmt,2,passw)
+    ibm_db.execute(stmt)
+    account = ibm_db.fetch_assoc(stmt)
+    print (account)
+    print(user,passw)
+    if account:
+            return redirect(url_for('stats'))
+    else:
+        return render_template('login.html', pred="Login unsuccessful. Incorrect username / password !") 
+      
+        
+@app.route('/stats')
+def stats():
+    '''sql = "SELECT blood FROM user group by blood"
+    stmt = ibm_db.prepare(conn, sql)
+    ibm_db.execute(stmt)
+    count = ibm_db.fetch_assoc(stmt)
+    print(count)'''
+    return render_template('stats.html',b=5,b1=2,b2=3,b3=4,b4=2,b5=1,b6=2,b7=1,b8=1)
+
+@app.route('/requester')
+def requester():
+    return render_template('request.html')
+
+
+@app.route('/requested',methods=['POST'])
+def requested():
+    bloodgrp = request.form['bloodgrp']
+    address = request.form['address']
+    print(address)
+    sql = "SELECT * FROM user WHERE blood=?"
+    stmt = ibm_db.prepare(conn, sql)
+    ibm_db.bind_param(stmt,1,bloodgrp)
+    ibm_db.execute(stmt)
+    data = ibm_db.fetch_assoc(stmt)
+    msg = "Need Plasma of your blood group for: "+address
+    while data != False:
+        print ("The Phone is : ", data["PHONE"])
+        url="https://www.fast2sms.com/dev/bulk?authorization=xCXuwWTzyjOD2ARd1EngbH3a7tKIq5PklJ8YSf0Lh4FQZecs9iNI1dSvuqprxFwCKYJXA5amQkBE36Rl&sender_id=FSTSMS&message="+msg+"&language=english&route=p&numbers="+str(data["PHONE"])
+        result=requests.request("GET",url)
+        print(result)
+        data = ibm_db.fetch_assoc(stmt)
+    return render_template('request.html', pred="Your request is sent to the concerned people.")
+    
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
+
